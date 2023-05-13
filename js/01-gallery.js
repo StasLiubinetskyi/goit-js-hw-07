745dd5baaec14560201fc02142fff0393c31a859
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-13 19:47:45 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector(".gallery-container");
+const galleryList = galleryContainer.querySelector(".gallery");
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return (
+        <li class="gallery__item">
+          {" "}
+          <a class="gallery__link" href="${original}">
+            {" "}
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+              data-source="${original}"
+            />{" "}
+          </a>{" "}
+        </li>
+      );
+    })
+    .join("");
+}
+
+galleryList.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
