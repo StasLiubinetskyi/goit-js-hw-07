14a755131e71dd2696378bd1da9ea6d2b3df2b9b
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-15 18:49:20 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,5 +3,48 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
+const galleryContainer = document.querySelector(".gallery");
+let modalInstance = null;
 
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}
 
+function renderGalleryItems() {
+  const galleryMarkup = galleryItems
+    .map((item) => createGalleryItemMarkup(item))
+    .join("");
+
+  galleryContainer.innerHTML = galleryMarkup;
+}
+
+function openModal(event) {
+  event.preventDefault(); // Заборона перенаправлення при кліку на зображення
+  const imageSource = event.target.dataset.source;
+  const modalContent = `<img src="${imageSource}" alt="" />`;
+  modalInstance = basicLightbox.create(modalContent);
+  modalInstance.show();
+  document.addEventListener("keydown", closeModalOnEscape);
+}
+
+function closeModalOnEscape(event) {
+  if (event.code === "Escape") {
+    modalInstance.close();
+    document.removeEventListener("keydown", closeModalOnEscape);
+  }
+}
+
+galleryContainer.addEventListener("click", openModal);
+
+renderGalleryItems();
