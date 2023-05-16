07f44f0cdb45f196f654be466ba00ab72513caf8
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-16 20:05:58 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+function renderGalleryItems() {
+  const galleryMarkup = galleryItems
+    .map((item) => createGalleryItemMarkup(item))
+    .join("");
+
+  galleryContainer.innerHTML = galleryMarkup;
+}
+
+renderGalleryItems();
+
+// Ініціалізація SimpleLightbox після створення і додання елементів галереї
+const gallery = new SimpleLightbox(".gallery__item a", {
+  captions: true,
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "top",
+});
